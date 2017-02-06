afab292e19f6e147e727b80fec79c50ffc31a3f2
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-06 18:44:20 +0700

    tambah def create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,14 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
 end
