b57d7d2cd8e949ac978645e69b03b5ffdc0d0757
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-07 22:33:43 +0700

    perbaikan ngeselin -_-

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @comments = Comment.where(post_id: @post)
   end
 
   def new
@@ -52,6 +53,7 @@ class PostsController < ApplicationController
   private
 
   def find_post
+    @post = Post.find(params[:id])
   end
 
   def post_params
